093107c6f92b23c9730fcb42f709322891e7f7b1
Ryuzaki47 <ryuzakikamisama@gmail.com>
2026-06-15 23:37:12 +0800

    Version Experiment 15

--- public/build/assets/chevron-left-CvCgIvKS.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./createLucideIcon-B2al4Iyg.js";const t=e("ChevronLeftIcon",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{t as C};
---
+++ public/build/assets/chevron-left-D74OTSmz.js
@@ -0,0 +1,1 @@
+import{c as e}from"./createLucideIcon-Bv2DOtW5.js";const t=e("ChevronLeftIcon",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{t as C};
